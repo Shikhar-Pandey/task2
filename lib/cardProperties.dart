2e68b7554584a84c386155a03a410e7dab99382b
Shikhar <shikharnew133@gmail.com>
2021-05-27 23:44:56 +0530

    Scroll feature needs Improvisation

--- lib/cardProperties.dart
+++ lib/cardProperties.dart
@@ -166,7 +166,7 @@ class DetailCard extends StatelessWidget {
                     flex: 1,
                     child: Container(
                       child: Image(
-                        image: AssetImage("assets/truck load.png"),
+                        image: AssetImage("assets/truckload.png"),
                       ),
                     ))
               ],
@@ -240,60 +240,3 @@ class DetailCard extends StatelessWidget {
     ]);
   }
 }
-
-//
-//
-// class TasksData extends ChangeNotifier {
-//   List<CardTile> cards = [];
-//
-//   void addTasks(
-//       String productType,
-//       String loadingPoint,
-//       String unloadingPoint,
-//       String truckPreference,
-//       String noOfTrucks,
-//       String weight,
-//       bool isPending,
-//       String comments,
-//       bool isCommentsEmpty,
-//       ) {
-//     cards.add(CardTile(
-//         loadingPoint: loadingPoint,
-//         unloadingPoint: unloadingPoint,
-//         productType: productType,
-//         truckPreference: truckPreference,
-//         noOfTrucks: noOfTrucks,
-//         weight: weight,
-//         isPending: isPending,
-//         comments: comments,
-//         isCommentsEmpty: isCommentsEmpty));
-//     notifyListeners();
-//   }
-// }
-// //
-// class MyAlertDialog extends StatelessWidget {
-//   final String title;
-//   final String content;
-//   final List<Widget> actions;
-//
-//   MyAlertDialog({
-//     this.title,
-//     this.content,
-//     this.actions = const [],
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return AlertDialog(
-//       title: Text(
-//         this.title,
-//         style: Theme.of(context).textTheme.title,
-//       ),
-//       actions: this.actions,
-//       content: Text(
-//         this.content,
-//         style: Theme.of(context).textTheme.body1,
-//       ),
-//     );
-//   }
-// }
--- lib/findLoad.dart
+++ lib/findLoad.dart
@@ -6,20 +6,21 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter/material.dart';
 import 'package:task_2/backend_connection.dart';
 import 'package:task_2/cardProperties.dart';
-String mapKey = "AIzaSyCTVVijIWofDrI6LpSzhUqJIF90X-iyZmE";
 
 class FindLoad extends StatefulWidget {
-
   @override
   _FindLoadState createState() => _FindLoadState();
 }
 
 class _FindLoadState extends State<FindLoad> {
-  var jsonData;
+  var  jsonData;
+
   Future<List<CardsModal>> getCardsData() async {
-    http.Response response = await http.get(
-        'http://ec2-52-53-40-46.us-west-1.compute.amazonaws.com:8080/load');
-    jsonData = json.decode(response.body);
+    http.Response response = await http.get('http://52.53.40.46:8080/load');
+    if(response.statusCode==200){
+      jsonData = json.decode(response.body);
+    }
+
     List<CardsModal> card = [];
 
     for (var json in jsonData) {
@@ -34,7 +35,7 @@ class _FindLoadState extends State<FindLoad> {
       cardsModal.status = json["status"];
       card.add(cardsModal);
     }
-    return card;
+    return  await card;
   }
 
   @override
@@ -42,181 +43,189 @@ class _FindLoadState extends State<FindLoad> {
     return SafeArea(
         child: Scaffold(
             backgroundColor: Color(0xFFF3F2F1),
-            body: Container(
-                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.0),
-                child: Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(color: Colors.white),
-                      child: Column(
-                        children: [
-                          Row(
+            body: ListView(
+              children: [
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Column(
                             children: [
-                              Expanded(
-                                  flex: 1,
-                                  child:
-                                  Icon(Icons.keyboard_backspace_rounded)),
-                              Expanded(
-                                flex: 9,
-                                child: Text(
-                                  "Find Load",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20),
-                                ),
-                              )
-                            ],
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Card(
-                              shape: RoundedRectangleBorder(
-                                side: BorderSide(color: Colors.black),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(35),
-                                ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                      flex: 1,
+                                      child:
+                                          Icon(Icons.keyboard_backspace_rounded)),
+                                  Expanded(
+                                    flex: 9,
+                                    child: Text(
+                                      "Find Load",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20),
+                                    ),
+                                  )
+                                ],
                               ),
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.circle,
-                                  color: Colors.green,
-                                  size: 20,
-                                ),
-                                title: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: "Enter loading point",
-                                      hintStyle:
-                                      TextStyle(color: Colors.black)),
-                                ),
-                                trailing:
-                                GestureDetector(child: Icon(Icons.close)),
-                              )),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Card(
-                              shape: RoundedRectangleBorder(
-                                side: BorderSide(color: Colors.black),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(35),
-                                ),
+                              SizedBox(
+                                height: 15,
                               ),
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.circle,
-                                  color: Colors.red,
-                                  size: 20,
-                                ),
-                                title: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: "Enter unloading point",
-                                      hintStyle:
-                                      TextStyle(color: Colors.black)),
-                                ),
-                                trailing: Icon(Icons.close),
-                              ))
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    Container(
-                        padding:
-                        EdgeInsets.symmetric(horizontal: 12, vertical: 15),
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Available Loads',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18),
-                                ),
-                                Container(
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 10, vertical: 2),
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20),
-                                      border: Border.all(color: Colors.indigo)),
-                                  child: Expanded(
-                                    child: Row(
-                                      mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                      children: [
-                                        Text(
-                                          "Filter",
-                                          style: TextStyle(fontSize: 14),
-                                        ),
-                                        Icon(
-                                          Icons.filter_alt_outlined,
-                                          color: Colors.indigo.shade300,
-                                        )
-                                      ],
+                              Card(
+                                  shape: RoundedRectangleBorder(
+                                    side: BorderSide(color: Colors.black),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(35),
                                     ),
                                   ),
-                                )
-                              ],
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
+                                  child: ListTile(
+                                    leading: Icon(
+                                      Icons.circle,
+                                      color: Colors.green,
+                                      size: 20,
+                                    ),
+                                    title: TextField(
+                                      decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                          hintText: "Enter loading point",
+                                          hintStyle:
+                                              TextStyle(color: Colors.black)),
+                                    ),
+                                    trailing:
+                                        GestureDetector(child: Icon(Icons.close)),
+                                  )),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Card(
+                                  shape: RoundedRectangleBorder(
+                                    side: BorderSide(color: Colors.black),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(35),
+                                    ),
+                                  ),
+                                  child: ListTile(
+                                    leading: Icon(
+                                      Icons.circle,
+                                      color: Colors.red,
+                                      size: 20,
+                                    ),
+                                    title: TextField(
+                                      decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                          hintText: "Enter unloading point",
+                                          hintStyle:
+                                              TextStyle(color: Colors.black)),
+                                    ),
+                                    trailing: Icon(Icons.close),
+                                  ))
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Container(
+                            padding:
+                                EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+                            decoration: BoxDecoration(color: Colors.white),
+                            child: ListView(shrinkWrap: true,
                               children: [
-                                FutureBuilder(
-                                    future: getCardsData(),
-                                    builder: (BuildContext context, AsyncSnapshot snapshot) {
-                                      print(snapshot.data);
-                                      if (snapshot.data == null) {
-                                        return Container(
-                                          child: Center(
-                                              child: SpinKitWave(
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Available Loads',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 10, vertical: 2),
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(20),
+                                          border: Border.all(color: Colors.indigo)),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Text(
+                                            "Filter",
+                                            style: TextStyle(fontSize: 14),
+                                          ),
+                                          Icon(
+                                            Icons.filter_alt_outlined,
+                                            color: Colors.indigo.shade300,
+                                          )
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    FutureBuilder(
+                                        future: getCardsData(),
+                                        builder: (BuildContext context,
+                                            AsyncSnapshot snapshot) {
+                                          if (snapshot.data == null) {
+                                            return Container(
+                                              child: Center(
+                                                  child: SpinKitWave(
                                                 color: Colors.lightBlueAccent,
                                                 size: 60,
-                                              )
-                                          ),
-                                        );
-                                      }
-                                      return ListView.builder(
-                                        reverse: false,
-                                        padding: EdgeInsets.symmetric(
-                                          horizontal: 10,
-                                        ),
-                                        itemCount: snapshot.data.length,
-                                        itemBuilder: (context, index) => DetailCard(
-                                          loadingPoint: snapshot.data[index].loadingPoint,
-                                          unloadingPoint: snapshot.data[index].unloadingPoint,
-                                          productType: snapshot.data[index].productType,
-                                          truckPreference: snapshot.data[index].truckType,
-                                          noOfTrucks: snapshot.data[index].noOfTrucks,
-                                          weight: snapshot.data[index].weight,
-                                          isPending: snapshot.data[index].status == 'pending'
-                                              ? true
-                                              : false,
-                                          comments: snapshot.data[index].comment,
-                                          isCommentsEmpty:
-                                          snapshot.data[index].comment == '' ? true : false,
-                                        ),
-                                      );
-                                    }),
+                                              )),
+                                            );
+                                          }
+                                          return ListView.builder(
+                                            reverse: false,
+                                            shrinkWrap: true,
+                                            padding: EdgeInsets.symmetric(
+                                              horizontal: 10,
+                                            ),
+                                            itemCount: snapshot.data.length,
+                                            itemBuilder: (BuildContext context, index) =>
+                                                DetailCard(
+                                              loadingPoint:
+                                                  snapshot.data[index].loadingPoint,
+                                              unloadingPoint: snapshot
+                                                  .data[index].unloadingPoint,
+                                              productType:
+                                                  snapshot.data[index].productType,
+                                              truckPreference:
+                                                  snapshot.data[index].truckType,
+                                              noOfTrucks:
+                                                  snapshot.data[index].noOfTrucks,
+                                              weight: snapshot.data[index].weight,
+                                              isPending:
+                                                  snapshot.data[index].status ==
+                                                          'pending'
+                                                      ? true
+                                                      : false,
+                                              comments:
+                                                  snapshot.data[index].comment,
+                                              isCommentsEmpty:
+                                                  snapshot.data[index].comment == ''
+                                                      ? true
+                                                      : false,
+                                            ),
+                                          );
+                                        }),
+                                  ],
+                                ),
                               ],
-                            ),
-
-                          ],
-                        )
-                    )
-                  ],
-                )
-            )
-        )
-    );
+                            ))
+                      ],
+                    )),
+              ],
+            )));
   }
 }
